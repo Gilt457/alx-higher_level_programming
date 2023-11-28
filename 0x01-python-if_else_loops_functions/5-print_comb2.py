c32a274feda98807eb3068e9a1360c9fc5c19be7
Gilt457 <lepatioborel@gmail.com>
2023-11-28 04:15:01 -0800

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-for number in range(100):
-    if number < 99:
-        print("{:02d}".format(number), end=", ")
-    else:
-        print(number)
+for num1 in range(10):
+    for num2 in range(10):
+        if num2 > num1 and num1 != 8:
+            print("{}{}".format(num1, num2), end=", ")
+            continue
+        elif num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
+            break
+
