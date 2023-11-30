505674b069b72200135a37cbccd40031b19c4e99
Gilt457 <lepatioborel@gmail.com>
2023-11-30 02:26:48 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     """Print the addition of all arguments"""
     import sys
 
-    sum = 0  
-    for index, argument in enumerate(sys.argv[1:]):  
-        sum += int(argument)
+    sum = 0
+    for index in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
     print("{}".format(sum))
