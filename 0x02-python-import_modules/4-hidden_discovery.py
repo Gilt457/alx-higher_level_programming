da23bc190495e2b9fc0600046d2d85dd74800b9d
Gilt457 <lepatioborel@gmail.com>
2023-11-30 02:38:07 -0800

    who are you

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,8 +4,7 @@ if __name__ == "__main__":
     """Print all names defined by hidden_4 module"""
     import hidden_4
 
-    concealed_names = dir(hidden_4)
-    for concealed_name in concealed_names:
-        if concealed_name[:2] != "__":
-            print(concealed_name)
-
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
