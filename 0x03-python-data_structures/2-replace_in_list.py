6065363889cd5c0f379a090243be7b2fc722c532
Gilt457 <lepatioborel@gmail.com>
2023-12-02 02:14:49 -0800

    updat

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,6 @@
+#!/usr/bin/python3
+
 def replace_in_list(my_list, idx, element):
-    # check if idx is valid
     if idx >= 0 and idx < len(my_list):
-        # replace the element at idx with the new element
         my_list[idx] = element
-    # return the modified or original list
     return my_list
