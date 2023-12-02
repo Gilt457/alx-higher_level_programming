5088a34573582764cf354668cd11a9cf391279e9
Gilt457 <lepatioborel@gmail.com>
2023-12-02 03:36:26 -0800

    Delete at

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def delete_at(my_list=None, idx=0):
+    """Delete an element from a list at a given index."""
+    if my_list is None:
+        my_list = []
+    if 0 <= idx < len(my_list):
+        my_list.pop(idx)
+    return my_list
