f3d8c0cecf0ebfa1e9e90dd4fb644b99b0fab73a
Gilt457 <lepatioborel@gmail.com>
2023-12-02 04:08:36 -0800

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Swap the values of two variables
+
 a = 89
 b = 10
-temp = a  # Store the value of a in a temporary variable
-a = b  # Assign the value of b to a
-b = temp  # Assign the value of temp to b
-print(f"a={a} - b={b}")  # Use f-strings to format the output
+temp = a
+a = b
+b = temp
+print(f"a={a} - b={b}")
