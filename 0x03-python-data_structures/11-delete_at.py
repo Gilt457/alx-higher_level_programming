883c115943a302f970a8897cd6979fbae3c042e2
Gilt457 <lepatioborel@gmail.com>
2023-12-02 04:18:30 -0800

    update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    """Delete an item at a specific position in a list."""
-    # Check if the index is valid
     if 0 <= idx < len(my_list):
-        # Loop through the list from the index to the end
-        for i in range(idx, len(my_list) - 1):
-            # Swap the current element with the next one
-            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
-        # Remove the last element of the list
-        my_list.pop()
-    # Return the modified list
-    return my_list
+        my_list = [x for i, x in enumerate(my_list) if i != idx]
+    return (my_list)
