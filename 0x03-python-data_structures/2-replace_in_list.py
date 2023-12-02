477bbe592b6cfab60d7cf0e45d91cc6f2488b53e
Gilt457 <lepatioborel@gmail.com>
2023-12-02 02:09:29 -0800

    update

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    # Validate the index
-    if not 0 <= idx <= len(my_list) - 1:
-        raise IndexError("Index out of range")
-
-    # Replace the element at the specified index
-    my_list[idx] = element
+    # check if idx is valid
+    if idx >= 0 and idx < len(my_list):
+        # replace the element at idx with the new element
+        my_list[idx] = element
+    # return the modified or original list
     return my_list
