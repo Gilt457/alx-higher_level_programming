a81967ed7b03687e5ba1ac266d009ef39ffbe4e1
Gilt457 <lepatioborel@gmail.com>
2023-12-18 03:04:09 -0800

    update

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,25 +1,20 @@
 #!/usr/bin/python3
-"""This module contains a function that divides two lists element."""
-
-
 def list_division(my_list_1, my_list_2, list_length):
-    """Divide two lists element by element and return a new list.
+    """Divide two lists element by element.
 
     Args:
-        my_list_1 (list): The first list of numbers.
-        my_list_2 (list): The second list of numbers.
+        my_list_1 (list): The first list.
+        my_list_2 (list): The second list.
         list_length (int): The number of elements to divide.
 
     Returns:
-        list: A new list of length list_length containing the quotients.
+        list: A new list of length list_length containing all the divisions.
     """
     new_list = []
     for i in range(list_length):
         try:
-            div = my_list_1[i] / my_list_2[i]
-        except (TypeError, ZeroDivisionError, IndexError) as error:
-            print(error)
-            div = 0
-        finally:
-            new_list.append(div)
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except (TypeError, ZeroDivisionError, IndexError) as e:
+            print(e)
+            new_list.append(0)
     return new_list
