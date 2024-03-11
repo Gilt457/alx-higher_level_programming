e38bad9697eb13cf58ff1eebfcea0136209c0b74
Gilt457 <lepatioborel@gmail.com>
2024-03-11 04:10:55 -0700

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-const argumentCount = process.argv.length;
-
-const argumentMessage = argumentCount > 2 ? `Argument${argumentCount > 3 ? 's' : ''} found` : 'No argument';
-console.log(argumentMessage);
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const arg = process.argv[2];
-const num = Number(arg);
-
-if (Number.isNaN(num)) {
-  console.error('Missing number of occurrences');
-} else {
-  Array.from({ length: num }, () => console.log('C is fun'));
-}
