181c2e932b2d5739803e74bfa6909eaf917f2746
Gilt457 <lepatioborel@gmail.com>
2024-03-12 11:12:50 -0700

    update

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,33 +1,11 @@
-#!/usr/bin/env node
-
-// Define the class
-class Rect {
-  // Constructor with width and height
-  constructor(width, height) {
-    this._width = width;
-    this._height = height;
-  }
-
-  // Getter for width
-  get width() {
-    return this._width;
-  }
-
-  // Setter for width
-  set width(newWidth) {
-    this._width = newWidth;
-  }
-
-  // Getter for height
-  get height() {
-    return this._height;
-  }
-
-  // Setter for height
-  set height(newHeight) {
-    this._height = newHeight;
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
   }
 }
 
-// Export the class
-module.exports = Rect;
+// Example usage:
+const myRectangle = new Rectangle(5, 3);
+console.log(myRectangle.width);  // Output: 5
+console.log(myRectangle.height); // Output: 3
